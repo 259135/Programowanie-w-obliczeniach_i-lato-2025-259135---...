b931edb2c66a945856a3129abb0455a20a171159
259135 <259135@edu.p.lodz.pl>
2025-04-21 15:32:06 +0200

    część kodu odpowiedzialnego za wyznaczanie wektora normalnego

--- cw2.py
+++ cw2.py
@@ -1,9 +1,34 @@
 import csv
+import math
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import pyransac3d
+from numpy.random import default_rng
+rng = default_rng()
 
 from sklearn.cluster import KMeans
+
+def ransac(dane, k, t, d):
+    iteracja=0
+    najlepsze_dopasowanie = null
+    najlepszy_blad = +inf
+    while iteracja<k :
+        wp = rng.permutation(dane)[0:2] #Wybrane Punkty
+        a = [wp[1][0] - wp[0][0], wp[1][1] - wp[0][1], wp[1][2] - wp[0][2]]
+        b = [wp[2][0] - wp[0][0], wp[2][1] - wp[0][1], wp[2][2] - wp[0][2]]
+
+        A = a[1] * b[2] - a[2] * b[1]
+        B = -(a[0] * b[2] - a[2] * b[0])
+        C = a[0] * b[1] - a[1] * b[0]
+        D = -A * wp[0][0] - B * wp[0][1] - C * wp[0][2]
+        axb = [A, B, C]
+
+        zaakceptowane_bliskie = []
+        for l in dane:
+            d = abs(A * l[0] + B * l[1] + C * l[2] + D)/math.sqrt(pow(A, 2) + pow(B, 2) + pow(C, 2))
+            if d < t:
+                zaakceptowane_bliskie.append(l)
+        if len(zaakceptowane_bliskie) > d:
+            
 
 
 sciezka1 = "C:\\Users\\ara22\\Desktop\\point_cl1.xyz"
@@ -34,8 +59,6 @@ k1Punkty = pArray[klaster1]
 k2Punkty = pArray[klaster2]
 k3Punkty = pArray[klaster3]
 
-plt.figure()
-
 plik1 = open(sciezka1, "w")
 for i in range(0,len(k1Punkty)): #dodawaj poszczególne elementy listy do pliku
     plik1.write(str(k1Punkty[i][0]) + " " + str(k1Punkty[i][1]) + " " + str(k1Punkty[i][2]) + "\n")
@@ -50,3 +73,5 @@ plik3 = open(sciezka3, "w")
 for i in range(0,len(k3Punkty)): #dodawaj poszczególne elementy listy do pliku
     plik3.write(str(k3Punkty[i][0]) + " " + str(k3Punkty[i][1]) + " " + str(k3Punkty[i][2]) + "\n")
 plik3.close()
+
+
